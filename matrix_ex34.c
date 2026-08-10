98293007348a59380cead91aada76faacae22d65
Black Bart <mhamidat37@gmail.com>
2026-08-10 17:59:51 +0100

    🔴 Level 3 - Exercise 34: "Matrix Transpose"
    The goal: The user enters a regular rectangular array and fills it in. Your program needs to create a new array called a "transferred array" with inverted dimensions! What does this mechanism do? It inverts the bottom of the rows to become columns, and the bottom of the columns to become rows.

---
+++ matrix_ex34.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(){ // Level03 | EX:34
+    printf("---Matrix Transpose---\n");
+    int n, m;
+    printf("Enter the line of the matrice(A):");
+    scanf("%d", &n);
+    printf("Enter the colomn of the matrice(A):");
+    scanf("%d", &m);
+    if (n < 0 || m < 0) {
+        printf("You can't beat my program, hahahaha.\n");
+        return 0;
+    }
+    int a[n][m];
+    printf("Fill in matrix (A):\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("line%d | colomn%d:", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("The Matrice(A):\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d", a[i][j]);
+            printf("\t");
+        }
+        printf("\n");
+    }
+    int b[m][n];
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            b[i][j] = a[j][i];
+        }
+    }
+    printf("Matrix flipping in progress..............\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d", b[i][j]);
+            printf("\t");
+        }
+        printf("\n");
+    }
+    printf("Transpose seccefuly!!\n");
+    return 0;
+}
